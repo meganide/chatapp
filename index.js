e71c29a0cc828afe332a696d4412e9ba4457cb9f
Meganide <renashassan@hotmail.com>
2022-12-11 00:06:16 +0100

    first

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const usernames = {};
+
+io.on('connection', (socket) => {
+  socket.on('add user', (username) => {
+    usernames[socket.id] = username;
+    io.emit('add user', username);
+    io.emit('users', usernames);
+  });
+  
+  
+  socket.on('Typing', () => {
+    socket.broadcast.emit('Typing', usernames[socket.id])
+  })
+
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', {
+      username: usernames[socket.id],
+      message: msg,
+    });
+  });
+
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected!');
+    io.emit('user disconnected', usernames[socket.id])
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('Server listening on port 3000...');
+});
